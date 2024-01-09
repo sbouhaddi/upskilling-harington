78a8b4d4fa54911829367b35017826356092b879
abou.soumahoro <gabonco@gmail.com>
2024-01-09 20:45:20 +0000

    Implement issue #1

---
+++ src/test/java/com/harington/upskilling/privateSchool/PrivateSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harington.upskilling.privateSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
